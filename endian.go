b10fe77d90f6d8c7aa8535119864aeee84735e9a
Sandeep Bagchi <sandeep.bagchi@gmail.com>
2021-08-12 15:36:20 +0530

    Create endian.go

---
+++ endian.go
@@ -0,0 +1,47 @@
+package libs
+
+import (
+	"encoding/binary"
+)
+
+func HostToNetworkShort(number uint16) uint16 {
+	if endian == binary.LittleEndian {
+		return (number << 8 & 0xff00) | (number >> 8 & 0xff)
+	}
+	return number
+}
+
+func NetworkToHostShort(number uint16) uint16 {
+	if endian == binary.LittleEndian {
+		return (number << 8 & 0xff00) | (number >> 8 & 0xff)
+	}
+	return number
+}
+
+func HostToNetwork(number uint32) uint32 {
+	if endian == binary.LittleEndian {
+		return uint32(HostToNetworkShort(uint16(number)))<<16 | uint32(HostToNetworkShort(uint16(number>>16)))
+	}
+	return number
+}
+
+func NetworkToHost(number uint32) uint32 {
+	if endian == binary.LittleEndian {
+		return uint32(NetworkToHostShort(uint16(number)))<<16 | uint32(NetworkToHostShort(uint16(number>>16)))
+	}
+	return number
+}
+
+func HostToNetworkLong(number uint64) uint64 {
+	if endian == binary.LittleEndian {
+		return uint64(HostToNetwork(uint32(number)))<<32 | uint64(HostToNetwork(uint32(number>>32)))
+	}
+	return number
+}
+
+func NetworkToHostLong(number uint64) uint64 {
+	if endian == binary.LittleEndian {
+		return uint64(NetworkToHost(uint32(number)))<<32 | uint64(NetworkToHost(uint32(number>>32)))
+	}
+	return number
+}
